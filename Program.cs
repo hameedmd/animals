c2201d309cf0974a6016b28e887c751bf6674959
hameedmd <52782158+hameedmd@users.noreply.github.com>
2019-07-11 12:49:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace animals_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new TerrestrialAnimalV1();
+            x.Move(2);
+        }
+    }
+}
