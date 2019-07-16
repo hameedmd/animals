d42fbf729b1e29fdaaebbd3138eb5e6f13392e82
hameedmd <52782158+hameedmd@users.noreply.github.com>
2019-07-16 09:11:10 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,14 +9,13 @@ namespace animals_task
             var animal1 = new TerrestrialAnimalV1(4, "Lion");
             var animal2 = new TerrestrialAnimalV1(4, "Giraffe");
             var animal3 = new TerrestrialAnimalV1(2, "Meerkat");
-            
-            var animal4 = new TerrestrialAnimalV2(0, "Shark");
-            var animal5 = new TerrestrialAnimalV2(100, "Centipede");
-            var animal6 = new TerrestrialAnimalV2(2, "Eagle");
-            
+
+            var animal4 = new TerrestrialAnimalV2();
+            var animal5 = new TerrestrialAnimalV2();
+            var animal6 = new TerrestrialAnimalV2();
+
             animal1.SayHi();
             animal1.Move();
-            
 
             animal2.SayHi();
             animal2.Move();
@@ -25,16 +24,17 @@ namespace animals_task
             animal3.Move();
             
 
-            animal4.SayHi(animal4.Name);
-            animal4.Move(animal4.NumberOfLegs);
+            animal4.SayHi("Shark");
+            animal4.Move(0);
             
             
-            animal5.SayHi(animal5.Name);
-            animal5.Move(animal5.NumberOfLegs);
+            animal5.SayHi("Centipede");
+            animal5.Move(100);
 
-            animal6.SayHi(animal6.Name);
-            animal6.Move(animal6.NumberOfLegs);
+            animal6.SayHi("Eagle");
+            animal6.Move(2);
 
         }
     }
 }
+
